e3a3b8c73a8dddd9b5ede906a0c58dd38b1a717a
Hans-J. Schmid <knock@myopendoor.de>
2025-04-16 18:32:40 +0200

    build

---
+++ build.sh
@@ -0,0 +1,36 @@
+cd ch01
+meson compile -C build
+cd ..
+cd ch02
+meson compile -C build
+cd ..
+cd ch03
+meson compile -C build
+cd ..
+cd ch04
+meson compile -C build
+cd ..
+cd ch05
+meson compile -C build
+cd ..
+cd ch06
+meson compile -C build
+cd ..
+cd ch07
+meson compile -C build
+cd ..
+cd ch08
+meson compile -C build
+cd ..
+cd ch09
+meson compile -C build
+cd ..
+cd ch10
+meson compile -C build
+cd ..
+cd ch11
+meson compile -C build
+cd ..
+cd ch12
+meson compile -C build
+cd ..
