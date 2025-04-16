1e53b1ee025c7dd3309e17130477e25c81ce878a
Hans-J. Schmid <knock@myopendoor.de>
2025-04-16 20:31:02 +0200

    ch01

--- ch01/main.cpp
+++ ch01/main.cpp
@@ -2,9 +2,12 @@
 #include <stdexcept>
 #include <vector>
 
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+
+// concrete (monomorphic) functions
 namespace ex1
 {
-    // ex1
     class array_of_ints
     {
         int data[10] = {};
@@ -22,6 +25,7 @@ namespace ex1
         }
     };
 
+    // monomorphic function
     void
     double_each_element(array_of_ints &arr)
     {
@@ -30,12 +34,21 @@ namespace ex1
             arr.at(i) *= 2;
         }
     }
-    // dex1
+
+    void
+    test()
+    {
+        array_of_ints arr;
+        double_each_element(arr);
+
+        // std::vector<int> vec = {1, 2, 3};
+        // double_each_element(vec);
+    }
 } // namespace ex1
 
+// classically polymorphic functions
 namespace ex2
 {
-    // ex2
     class container_of_ints
     {
       public:
@@ -45,37 +58,32 @@ namespace ex2
 
     class array_of_ints : public container_of_ints
     {
-        int data[10] = {};
-
       public:
         int
         size() const override
         {
             return 10;
         }
+
         int &
         at(int i) override
         {
             return data[i];
         }
+
+      private:
+        int data[10] = {};
     };
 
     class list_of_ints : public container_of_ints
     {
-        struct node
-        {
-            int   data;
-            node *next;
-        };
-        node *head_ = nullptr;
-        int   size_ = 0;
-
       public:
         int
         size() const override
         {
             return size_;
         }
+
         int &
         at(int i) override
         {
@@ -90,9 +98,28 @@ namespace ex2
             }
             return p->data;
         }
-        ~list_of_ints();
+
+        ~list_of_ints()
+        {
+            for (node *next, *p = head_; p != nullptr; p = next)
+            {
+                next = p->next;
+                delete p;
+            }
+        }
+
+      private:
+        struct node
+        {
+            int   data;
+            node *next;
+        };
+
+        node *head_ = nullptr;
+        int   size_ = 0;
     };
 
+    // polymorphic function
     void
     double_each_element(container_of_ints &arr)
     {
@@ -110,18 +137,13 @@ namespace ex2
 
         list_of_ints lst;
         double_each_element(lst);
-    }
-    // dex2
-    list_of_ints::~list_of_ints()
-    {
-        for (node *next, *p = head_; p != nullptr; p = next)
-        {
-            next = p->next;
-            delete p;
-        }
+
+        // std::vector<int> vec = {1, 2, 3};
+        // double_each_element(vec);
     }
 } // namespace ex2
 
+// generic programming
 namespace ex3
 {
     class container_of_ints
@@ -133,58 +155,65 @@ namespace ex3
 
     class array_of_ints : public container_of_ints
     {
-        int data[10] = {};
-
       public:
         int
         size() const override
         {
             return 10;
         }
+
         int &
         at(int i) override
         {
             return data[i];
         }
+
+      private:
+        int data[10] = {};
     };
 
     class list_of_ints : public container_of_ints
     {
-        struct node
-        {
-            int   data;
-            node *next;
-        };
-        node *head_ = nullptr;
-        int   size_ = 0;
-
       public:
         int
         size() const override
         {
             return size_;
         }
+
         int &
         at(int i) override
         {
             node *p = head_;
+
             for (int j = 0; p != nullptr && j < i; ++j)
             {
                 p = p->next;
             }
+
             return *(p ? &p->data : throw std::out_of_range("at"));
         }
-    };
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wsign-compare"
-    // ex3
-    template <class ContainerModel>
-    void
-    double_each_element(ContainerModel &arr)
-    {
-        for (int i = 0; i < arr.size(); ++i)
+
+      private:
+        struct node
         {
-            arr.at(i) *= 2;
+            int   data;
+            node *next;
+        };
+
+        node *head_ = nullptr;
+        int   size_ = 0;
+    };
+
+    // generic function
+    // `ContainerModel` is a model of the `Container` concept.
+    template <typename ContainerModel>
+    void
+    double_each_element(ContainerModel &cm)
+    {
+        for (int i = 0; i < cm.size(); ++i)
+        {
+            cm.at(i) *= 2;
         }
     }
 
@@ -199,77 +228,67 @@ namespace ex3
 
         std::vector<int> vec = {1, 2, 3};
         double_each_element(vec);
-    }
-// dex3
-#pragma GCC diagnostic pop
-    void
-    test2()
-    {
-        // ex4
+
         std::vector<double> vecd = {1.0, 2.0, 3.0};
         double_each_element(vecd);
-        // dex4
     }
 } // namespace ex3
 
-namespace ex5
+// more examples of generic algorithms
+namespace ex4
 {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-variable"
-    // ex5
-    template <class Container>
+    template <typename Container>
     int
     count(const Container &container)
     {
         int sum = 0;
+
         for (auto &&elt : container)
         {
             sum += 1;
         }
+
         return sum;
     }
-// dex5
-#pragma GCC diagnostic pop
 
-    // ex6
-    template <class Container, class Predicate>
+    template <typename Container, typename Predicate>
     int
     count_if(const Container &container, Predicate pred)
     {
         int sum = 0;
-        for (auto &&elt : container)
+
+        for (auto &&elt : container) // forwarding reference takes everything
         {
+            // we never need to specify whether pred takes its argument by value or by reference
             if (pred(elt))
             {
                 sum += 1;
             }
         }
+
         return sum;
     }
-    // dex6
 
     void
     test()
     {
-        // ex7
         std::vector<int> v = {3, 1, 4, 1, 5, 9, 2, 6};
 
         assert(count(v) == 8);
 
-        int number_above = count_if(v, [](int e) { return e > 5; });
-        int number_below = count_if(v, [](int e) { return e < 5; });
+        int number_above = count_if(v, [](auto elem) { return elem > 5; });
+        int number_below = count_if(v, [](auto elem) { return elem < 5; });
 
         assert(number_above == 2);
         assert(number_below == 5);
-        // dex7
     }
-} // namespace ex5
+} // namespace ex4
 
 int
 main()
 {
+    ex1::test();
     ex2::test();
     ex3::test();
-    ex3::test2();
-    ex5::test();
+    ex4::test();
 }
